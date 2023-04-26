61982ca96745972b83ecd23a1ca27677e2c349b5
Luis Liquin <luisliquin@outlook.com.ar>
2023-04-26 12:50:44 -0300

    cambios

---
+++ clases/04/ValidPass/ValidPass/Program.cs
@@ -0,0 +1,34 @@
+﻿//declaracion de las varaibles
+string sPassword = "usSystem-2022";
+int iIntentos;
+
+for (iIntentos=0; iIntentos < 5; iIntentos++)
+{
+    Console.Write("Ingrese la contraseña: ");
+    string input = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("No ha ingresado ninguna contraseña. Por favor, inténtelo de nuevo.");
+        iIntentos--;
+        continue;
+    }
+
+    if (input == sPassword)
+    {
+        Console.WriteLine("Bienvenido al sistema.");
+        break;
+    }
+    else
+    {
+        int remainingAttempts = 4 - iIntentos;
+
+        Console.WriteLine($"Contraseña incorrecta. Quedan {remainingAttempts} intentos.");
+    }
+}
+
+if (iIntentos == 5)
+{
+    Console.WriteLine("No es posible acceder al sistema. La aplicación se cerrará.");
+    Environment.Exit(0);
+}
